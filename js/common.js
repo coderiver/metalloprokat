caab91e5c5f35de717f6113e7f05dd556af5b28b
hraboviyvadim <hraboviyvadim@gmail.com>
2014-11-26 13:09:27 +0200

    catelog_1, catalog_2 added

--- js/common.js
+++ js/common.js
@@ -90,5 +90,47 @@ head.ready(function() {
 		}
 	)
 
+// tabs
+	
+	function tab() {
+       $(".js-tab").each(function(){
+         var tab_link = $(this).find("a");
+         var tab_item = $(this).find("li");
+         var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+         tab_cont.hide();
+         tab_item.first().addClass("is-active");
+         $(this).parents(".js-tab-group").find(".js-tab1").show();
+         tab_link.on("click", function() {
+             var index = $(this).attr("href");
+             tab_item.removeClass("is-active");
+             $(this).parent().addClass("is-active");
+             tab_cont.hide();
+             $(this).parents(".js-tab-group").find("."+index).show();
+             return false;
+          });
+       });
+  	}
+  	tab();
+
+ // scrollpane
+ 
+ 	$('.js-scroll').each(function(){
+		$(this).jScrollPane();
+		var api = $(this).data('jsp');
+		var throttleTimeout;
+
+		$(window).resize(function(){
+			if (!throttleTimeout) {
+				throttleTimeout = setTimeout(
+					function()
+					{
+						api.reinitialise();
+						throttleTimeout = null;
+					},
+					50
+				);
+			}
+		});
+	});
 
 });
--- js/init.js
+++ js/init.js
@@ -2,4 +2,6 @@ head.load("js/lib/jquery.js",
 					"js/lib/cycle.js",
 					"js/lib/jquery.form-validator.js",
 					"js/lib/jquery.fancybox.js",
+					"js/lib/jquery.jscrollpane.min.js",
+					"js/lib/jquery.mousewheel.js",
 					"js/common.js");
